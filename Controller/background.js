48dd0add95a8730dd3c50dea1eed09ff94f3a6d5
Lewis Fleming SE2018 <lewis.fleming.2018@uni.strath.ac.uk>
2022-03-14 12:39:09 +0000

    background updated to initialize settings object store in the database to hold user settings

--- Controller/background.js
+++ Controller/background.js
@@ -13,16 +13,35 @@ function createDB() {
 
     request.onupgradeneeded = function(e){
         db = e.target.result
-        const hItems = db.createObjectStore("history_items", {keyPath: "domain"})
+        db.createObjectStore("blacklist", {keyPath: "domain"})
+        db.createObjectStore("extension_settings", {keyPath: "id"})
         console.log(`upgrade is called database name: ${db.name} version: ${db.version}`)
     }
 
     request.onsuccess = function(e){
         db = e.target.result
-        console.log(`success is called database name: ${db.name} version: ${db.version}`)
+        var time = new Date()
+        console.log(`success is called database name: ${db.name} version: ${db.version} time: ${time.toUTCString()}`)
+        initSettings(db)
     }
 
     request.onerror = function(e){
         console.log(`error: ${e.target.error}`)
+    }
+}
+
+function initSettings(db) {
+    var transaction = db.transaction(["extension_settings"], "readwrite")
+    transaction.oncomplete = function(event) {
+        console.log("[IDB] Transaction Complete")
+    }
+    transaction.onerror = function(event) {
+        console.log(`[IDB] ${event.target.error}`)
+    }
+
+    var objectStore = transaction.objectStore("extension_settings")
+    var objectStoreRequest = objectStore.add({ id: "1", consent: "true"})
+    objectStoreRequest.onsuccess = function(event) {
+        console.log("[IDB] Request Successful")  //TODO - Change error message to something more meaningful
     }
 }
