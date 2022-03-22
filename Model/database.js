77ec1dac04f5638fde0309d23105e9addb2bcdb2
Lewis <gkb18158@uni.strath.ac.uk>
2022-03-22 10:11:25 +0000

    Added init settings function to fix bug when initialising setttings

--- Model/database.js
+++ Model/database.js
@@ -10,52 +10,53 @@ IDBrequest.onsuccess = function(event) {
     db = IDBrequest.result
     var transaction = db.transaction(["extension_settings"], "readwrite")
     transaction.oncomplete = function(event) {
-        console.log("[IDB] Transaction Complete")
+        console.log("[IDB] Transaction Complete: Consent checked succesfully")
     }
     transaction.onerror = function(event) {
-        console.log("[IDB] error")
+        console.log(`[IDB] ${event.target.error}`)
     }
 
     var objectStore = transaction.objectStore("extension_settings")
     var objectStoreRequest = objectStore.getAll()
     objectStoreRequest.onsuccess = function(event) {
         var data = objectStoreRequest.result
-        if(data[0].consent === "true") {
-            console.log("CONSENT GIVEN")
+        if(!data.length) {
+            initSettings(db, () =>  {
+                runExtension()
+            })
+        } else if(data[0].consent === "true") {
             runExtension()
-        } else {
-            console.log("CONSENT NOT GIVEN")
-        }
-        console.log("[IDB] Request Successful")  //TODO - Change error message to something more meaningful
+        } 
+        console.log("[IDB] Request Successful: Checking consent")  
     }
 }
 IDBrequest.onerror = function(event) {
-    console.log("somethings wrong") //TODO - Change error message to something more meaningful
+    console.log(`[IDB] ${event.target.error}`) 
 }
 
 function getData() {
     if(db === null) {
-        console.log("somethings wrong") //TODO - Change error message to something more meaningful
+        console.log("[IDB] Error: Database not Initialised")
     } else {
         return new Promise((resolve, reject) => {
             var transaction = db.transaction(["blacklist"], "readonly")
 
             transaction.oncomplete = function(event) {
-                console.log("[IDB] Transaction Complete")
+                console.log("[IDB] Transaction Complete: Data got from the DB")
             }
             transaction.onerror = function(event) {
-                reject("[IDB] error: somethings wrong") //TODO - Change error message to something more meaningful
+                reject(`[IDB] ${event.target.error}`) 
             }
 
             var objectStore = transaction.objectStore("blacklist")
             var getAllRequest = objectStore.getAll()
             
             getAllRequest.onsuccess = function(event) {
-                console.log("[IDB] Data Request Complete")
+                console.log("[IDB] Data Request Complete: Getting data from DB")
                 resolve(getAllRequest.result)
             }
             getAllRequest.onerror = function(event) {
-                reject("[IDB] error: somethings wrong") //TODO - Change error message to something more meaningful
+                reject(`[IDB] ${event.target.error}`) 
             }
         })
     }
@@ -64,15 +65,34 @@ function getData() {
 function addData(data) {
     var transaction = db.transaction(["blacklist"], "readwrite")
     transaction.oncomplete = function(event){
-        console.log("Transaction completed")  //TODO - Change error message to something more meaningful
+        console.log("[IDB] Transaction Complete: Data added successfully")  //TODO - Change error message to something more meaningful
     }
     transaction.onerror = function(event){
-        console.log("somethings wrong") //TODO - Change error message to something more meaningful
+        console.log(`[IDB] ${event.target.error}`) 
     }
 
     var objectStore = transaction.objectStore("blacklist")
     var objectStoreRequest = objectStore.add(data)
     objectStoreRequest.onsuccess = function(event) {
-        console.log("Request successful")  //TODO - Change error message to something more meaningful
+        console.log("[IDB] Request Successful: Adding data to DB")  //TODO - Change error message to something more meaningful
+    }
+}
+
+function initSettings(db, callback) {
+    var transaction = db.transaction(["extension_settings"], "readwrite")
+    transaction.oncomplete = function(event) {
+        console.log("[IDB] Transaction Complete: Settings Initialised")
+    }
+    transaction.onerror = function(event) {
+        console.log(`[IDB] ${event.target.error}`)
+    }
+
+    var objectStore = transaction.objectStore("extension_settings")
+    var objectStoreRequest = objectStore.add({ id: "1", consent: "true"})
+    objectStoreRequest.onsuccess = function(event) {
+        console.log("[IDB] Request Successful: Updating settings") 
+        if(callback) {
+            callback()
+        }
     }
 }
